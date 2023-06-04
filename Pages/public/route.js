1cf3854e10743b53ad2f7c9f5278f138c62de17d
JanushanJegan <106579444+JanushanJegan@users.noreply.github.com>
2023-06-04 20:12:55 +0100

    sql database + map upgrade + selection
    I used the mysql database as temporary database as soon as the other database is finished I will switch

--- Pages/public/route.js
+++ Pages/public/route.js
@@ -1,6 +1,95 @@
 var map = L.map("map").setView([55.8597930, -4.3226758], 13);
 
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors",
-    maxZoom: 18,
-  }).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors",
+  maxZoom: 18,
+}).addTo(map);
+
+var icon = L.icon({
+  iconUrl: 'Bus.png',
+  iconSize: [30, 30],
+  iconAnchor: [10, 10]
+});
+
+async function fetchBusStops() {
+  try {
+    const response = await fetch('/getbusstops');
+    if (!response.ok) {
+      throw new Error('Failed to fetch bus stops');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+async function addBusStopsToMap() {
+  const busStops = await fetchBusStops();
+  
+  if(busStops !== null){
+    busStops.forEach(stop => {
+      var marker = L.marker([stop.latitude, stop.longitude], { icon: icon }).addTo(map);
+      marker.on('click', function () {
+        var lat = marker.getLatLng().lat;
+        var lng = marker.getLatLng().lng;
+
+        var data = {
+          lat: lat,
+          lng: lng
+        };
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/your-server-script', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            console.log('Coordinates sent to the server');
+          }
+        };
+        xhr.send(JSON.stringify(data));
+      });
+    });
+  }
+}
+async function fetchBusStopsForUsers() {
+  try {
+    const response = await fetch('/getbusstopsforusers');
+    if (!response.ok) {
+      throw new Error('Failed to fetch bus stops for users');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+async function populateDropdowns() {
+  const busStopsForUsers = await fetchBusStopsForUsers();
+  
+  if(busStopsForUsers !== null){
+    let dropdown1 = document.getElementById('input1');
+    let dropdown2 = document.getElementById('input2');
+    
+    busStopsForUsers.forEach(stop => {
+      let option1 = document.createElement('option');
+      let option2 = document.createElement('option');
+      
+      option1.text = `${stop.busstopname}, ${stop.bearing}, ${stop.busstopindicator}`;
+      option2.text = `${stop.busstopname}, ${stop.bearing}, ${stop.busstopindicator}`;
+      
+      dropdown1.add(option1);
+      dropdown2.add(option2);
+    });
+  }
+}
+
+
+
+
+// Call the function when the script runs
+addBusStopsToMap();
+populateDropdowns();
--- Pages/server.js
+++ Pages/server.js
@@ -1,24 +1,69 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
-const uri='mongodb+srv://alanbiju1234:Soidontforget@synoptic.q5e7ldv.mongodb.net/?retryWrites=true&w=majority'
+const { createPool } = require('mysql')
 
-async function connect(){
-    try{
-        await mongoose.connect(uri)
-        console.log("Connected to mongoDB") 
-    }catch (error){
-        console.error(error);
-    }
-}
-app.use(express.static('public'));
-
-
-connect();
-
-app.listen(3000,()=>{
-    console.log('listening on port 3000')
+const pool = createPool({
+  host: "localhost",
+  user: "sqluser",
+  password: "password",
+  database: "gova-bus" 
 })
 
+app.use(express.static('public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/your-server-script', (req, res) => {
+  const { lat, lng } = req.body;
+  console.log(lat,lng)
+  pool.query(
+    "SELECT * FROM busstops WHERE latitude = ? AND longitude = ?",
+    [lat, lng],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.sendStatus(500); 
+      } else {
+        res.send(result); 
+        console.log(result)
+      }
+    }
+  );
+});
+
+app.get('/getbusstops', (req, res) => {
+  pool.query(
+    "SELECT latitude, longitude FROM busstops",
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.sendStatus(500);
+      } else {
+        res.send(result); 
+      }
+    }
+  );
+});
+
+app.get('/getbusstopsforusers', (req, res) => {
+  pool.query(
+    "SELECT busstopname, bearing, busstopindicator FROM busstops",
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.sendStatus(500); 
+      } else {
+        res.send(result); 
+      }
+    }
+  );
+});
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
