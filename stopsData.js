7860bf8f78e1ddb6b208e1f9f2768e8d2f1d2ef5
Talan Oates <129100141+TalanOatis@users.noreply.github.com>
2023-06-05 01:25:21 +0100

    Add files via upload

---
+++ stopsData.js
@@ -0,0 +1,28 @@
+const stopsData = [
+    {
+      "id": 1,
+      "stop_name": "Bus stop A",
+      "stop_destination": "Library",
+      "stop_arrival": "14:29",
+    },
+    {
+      "id": 2,
+      "stop_name": "Bus stop B",
+      "stop_destination": "Town",
+      "stop_arrival": "14:36",
+    },
+    {
+      "id": 3,
+      "stop_name": "Bus stop C",
+      "stop_destination": "Other",
+      "stop_arrival": "14:42",
+    },
+    {
+      "id": 4,
+      "stop_name": "Bus stop D",
+      "stop_destination": "Library",
+      "stop_arrival": "14:57",
+    }
+  ];
+  
+  module.exports = stopsData;
